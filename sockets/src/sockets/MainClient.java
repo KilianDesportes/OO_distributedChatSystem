fe5c8fc7934ebdaaa08cfb976bb5d67269ba8358
Kilian <kilian.desportes@gmail.com>
2019-11-29 18:13:37 +0100

    main modif

--- sockets/src/sockets/MainClient.java
+++ sockets/src/sockets/MainClient.java
@@ -5,27 +5,24 @@ import java.util.Scanner;
 
 public class MainClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 
 
 		 threadSend th = new threadSend();
 		 th.start();
 		 Scanner scan = new Scanner(System.in);
 		 
-		 th.login("Kilian");
+		 System.out.println("What is your pseudo?");
+		 String s = scan.nextLine();
+		 
+		 th.login(s);
 		 
 		 while(true){
 
-			 System.out.println("Enter your message");
-			 String s = scan.nextLine();
-			 InetAddress adr =null;
-			try {
-				adr = InetAddress.getLocalHost();
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			}
+			 System.out.println("Your message");
+			 s = scan.nextLine();
+			 InetAddress adr = InetAddress.getByName("10.32.45.187");
 			 th.sendMessage(s, adr);
-			 s="";
 			 
 		 }
 
