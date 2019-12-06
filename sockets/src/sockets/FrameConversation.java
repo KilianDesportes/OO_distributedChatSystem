80b2647c28e4af88a3968e5eb67cf778b7697c72
YanisI <yanis.imekraz34@gmail.com>
2019-12-06 14:16:57 +0100

    heure

---
+++ sockets/src/sockets/FrameConversation.java
@@ -0,0 +1,24 @@
+package sockets;
+
+import java.awt.*;
+import java.net.*;
+import javax.swing.*;
+
+public class FrameConversation  {
+	
+	JFrame convFrame;
+	
+	
+	public FrameConversation(String pseudo/*,InetAddress addIp*/)
+	{
+			
+		convFrame = new JFrame(pseudo);
+		convFrame .setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		convFrame.setSize(new Dimension(350,500));
+		
+		conv.
+		
+		convFrame.setVisible(true);
+	}
+
+}
